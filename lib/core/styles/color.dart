62b375186295843efb9cd14b36b32a38672a5e0f
wahyu-loratech <wahyu@loratechai.com>
2022-06-21 10:40:46 +0700

    adding color palette

---
+++ lib/core/styles/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class COLORS {
+  static const primary = Color(0xFF006EF2);
+  static const up = Color(0xFF008753);
+  static const down = Color(0xFFC33335);
+  static const text = Color(0xFF222E41);
+  static const dim = Color(0xFF999999);
+  static const background = Color(0xFFF4F4F4);
+  static const background2 = [Color(0xFF006EF2), Color(0xFF004396)];
+}
