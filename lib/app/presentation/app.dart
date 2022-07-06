b7394c03512a889fb415f95ca73d0258419e39db
Krishna Gupta <krishna@asklora.ai>
2022-07-06 16:03:29 +0800

    code clean up

--- lib/app/presentation/app.dart
+++ lib/app/presentation/app.dart
@@ -29,7 +29,7 @@ class App extends StatelessWidget {
         initialRoute: App.route,
         home: BlocProvider(
           create: (_) => AppBloc(
-            tokenValidation: tokenValidator,
+            tokenValidator: tokenValidator,
           )..add(AppLaunched()),
           child: BlocListener<AppBloc, AppState>(
             listenWhen: (previous, current) =>
