da1538bb3c48c5b8487921fc2c39aa4229f86cd0
wahyu-loratech <wahyu@loratechai.com>
2022-07-13 15:42:48 +0700

    refactor code

--- lib/feature/user/account/presentation/upgrade_account/upgrade_account_screen.dart
+++ lib/feature/user/account/presentation/upgrade_account/upgrade_account_screen.dart
@@ -54,6 +54,10 @@ class UpgradeAccountScreen extends StatelessWidget {
           key: const Key('signing_broker_agreement_step'),
           controller: _pageViewController,
         ),
+        TrustedContactForm(
+          key: const Key('trusted_contact_step'),
+          controller: _pageViewController,
+        ),
         RiskDisclosureForm(
             key: const Key('risk_disclosure_step'),
             controller: _pageViewController),
@@ -64,38 +68,6 @@ class UpgradeAccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PageController _pageViewController =
-        PageController(initialPage: initialPage);
-
-    List<Widget> _pages = [
-      BasicInformationForm(
-        key: const Key('basic_information_step'),
-        controller: _pageViewController,
-      ),
-      AddressProofForm(
-        key: const Key('address_proof_step'),
-        controller: _pageViewController,
-      ),
-      FinancialProfileForm(controller: _pageViewController),
-      DisclosuresAffiliationsForm(
-        key: const Key('disclosures_affiliations_step'),
-        controller: _pageViewController,
-      ),
-      SigningAgreementTaxForm(
-          key: const Key('signing_agreement_tax_step'),
-          controller: _pageViewController),
-      SigningBrokerAgreementsForm(
-        key: const Key('signing_broker_agreement_step'),
-        controller: _pageViewController,
-      ),
-      TrustedContactForm(
-        key: const Key('trusted_contact_step'),
-        controller: _pageViewController,
-      ),
-      RiskDisclosureForm(
-          key: const Key('risk_disclosure_step'),
-          controller: _pageViewController),
-    ];
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
