2d8099dc04a960fb4bcbed2b3d8e526c88735fe1
askloraheri <heri.susanto@loratechai.com>
2022-07-13 17:26:25 +0700

    Refactor basic information on date of birth field, add scenario for hongkong permanent residence and add note for us resident

--- lib/feature/user/account/presentation/upgrade_account/basic_information_form.dart
+++ lib/feature/user/account/presentation/upgrade_account/basic_information_form.dart
@@ -5,16 +5,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/presentation/custom_date_picker.dart';
 import '../../../../../core/presentation/custom_dropdown.dart';
 import '../../../../../core/presentation/custom_phone_number_input.dart';
+import '../../../../../core/presentation/custom_text.dart';
 import '../../../../../core/presentation/custom_text_button.dart';
 import '../../../../../core/presentation/custom_text_input.dart';
 import '../../../../../core/presentation/question_widget.dart';
 import '../../bloc/account_bloc.dart';
 import '../../bloc/basic_information/bloc/basic_information_bloc.dart';
+import 'not_eligible_screen.dart';
 
 class BasicInformationForm extends StatelessWidget {
   final PageController controller;
 
-  const BasicInformationForm({
+  final _scrollController = ScrollController();
+
+  BasicInformationForm({
     Key? key,
     required this.controller,
   }) : super(key: key);
@@ -25,6 +29,7 @@ class BasicInformationForm extends StatelessWidget {
       children: [
         Expanded(
             child: SingleChildScrollView(
+          controller: _scrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -41,7 +46,7 @@ class BasicInformationForm extends StatelessWidget {
             ],
           ),
         )),
-        _nextButton()
+        _nextButton(context)
       ],
     );
   }
@@ -111,7 +116,8 @@ class BasicInformationForm extends StatelessWidget {
 
   Widget _datePicker() =>
       BlocBuilder<BasicInformationBloc, BasicInformationState>(
-        buildWhen: ((previous, current) => false),
+        buildWhen: ((previous, current) =>
+            previous.dateOfBirth != current.dateOfBirth),
         builder: (context, state) {
           final DateTime dateTime = DateTime.now();
           return CustomDatePicker(
@@ -161,15 +167,54 @@ class BasicInformationForm extends StatelessWidget {
           previous.isHongKongPermanentResident !=
           current.isHongKongPermanentResident),
       builder: (context, state) {
-        return QuestionWidget(
-          key: const Key('account_is_hongkong_permanent_resident_question'),
-          padding: const EdgeInsets.only(top: 10),
-          questionText: 'Hong Kong Permanent Resident',
-          options: const ['Yes', 'No'],
-          selectedAnswer: state.isHongKongPermanentResident ? 'Yes' : 'No',
-          onSelected: (value) => context.read<BasicInformationBloc>().add(
-              BasicInformationIsHongKongPermanentResidentChanged(
-                  value == 'Yes' ? true : false)),
+        return Column(
+          children: [
+            QuestionWidget(
+              key: const Key('account_is_hongkong_permanent_resident_question'),
+              padding: const EdgeInsets.only(top: 10),
+              questionText: 'Hong Kong Permanent Resident',
+              options: const ['Yes', 'No'],
+              selectedAnswer: state.isHongKongPermanentResident != null
+                  ? (state.isHongKongPermanentResident! ? 'Yes' : 'No')
+                  : null,
+              onSelected: (value) async {
+                context.read<BasicInformationBloc>().add(
+                    BasicInformationIsHongKongPermanentResidentChanged(
+                        value == 'Yes' ? true : false));
+                Future.delayed(
+                    const Duration(milliseconds: 200),
+                    () => _scrollController.animateTo(
+                          _scrollController.position.maxScrollExtent,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        ));
+              },
+            ),
+            if (state.isHongKongPermanentResident != null &&
+                state.isHongKongPermanentResident!)
+              _idNumberInput(context),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _idNumberInput(BuildContext context) {
+    return BlocBuilder<BasicInformationBloc, BasicInformationState>(
+      buildWhen: (previous, current) =>
+          previous.isHongKongPermanentResident !=
+          current.isHongKongPermanentResident,
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: CustomTextInput(
+              key: const Key('account_id_number_input'),
+              labelText: 'ID Number *',
+              initialValue: state.idNumber,
+              onChanged: (value) => context
+                  .read<BasicInformationBloc>()
+                  .add(BasicInformationIdNumberChanged(value)),
+              hintText: 'Enter your ID number'),
         );
       },
     );
@@ -180,25 +225,57 @@ class BasicInformationForm extends StatelessWidget {
       buildWhen: ((previous, current) =>
           previous.isUnitedStateResident != current.isUnitedStateResident),
       builder: (context, state) {
-        return QuestionWidget(
-          key: const Key('account_is_united_state_resident_question'),
-          padding: const EdgeInsets.only(top: 10, bottom: 20),
-          questionText: 'US Resident Check',
-          options: const ['Yes', 'No'],
-          selectedAnswer: state.isUnitedStateResident ? 'Yes' : 'No',
-          onSelected: (value) => context.read<BasicInformationBloc>().add(
-              BasicInformationIsUnitedStateResidentChanged(
-                  value == 'Yes' ? true : false)),
+        return Column(
+          children: [
+            QuestionWidget(
+              key: const Key('account_is_united_state_resident_question'),
+              padding: const EdgeInsets.only(top: 10),
+              questionText: 'US Resident Check',
+              options: const ['Yes', 'No'],
+              selectedAnswer: state.isUnitedStateResident != null
+                  ? (state.isUnitedStateResident! ? 'Yes' : 'No')
+                  : null,
+              onSelected: (value) => context.read<BasicInformationBloc>().add(
+                  BasicInformationIsUnitedStateResidentChanged(
+                      value == 'Yes' ? true : false)),
+            ),
+            if (state.isUnitedStateResident != null &&
+                state.isUnitedStateResident!)
+              _usResidentsNote()
+          ],
         );
       },
     );
   }
 
-  Widget _nextButton() => Padding(
+  Widget _usResidentsNote() {
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomText(
+            '*',
+            type: FontType.smallNote,
+          ),
+          Expanded(
+            child: CustomText(
+              'I am a US tax resident, green card holder, or US citizen',
+              type: FontType.smallNote,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _nextButton(BuildContext context) => Padding(
         padding: const EdgeInsets.only(top: 10.0),
         child: BlocBuilder<BasicInformationBloc, BasicInformationState>(
           buildWhen: (previous, current) =>
-              previous.enableNextButton() != current.enableNextButton(),
+              previous.enableNextButton() != current.enableNextButton() ||
+              previous.isHongKongPermanentResident !=
+                  current.isHongKongPermanentResident,
           builder: (context, state) {
             return CustomTextButton(
               key: const Key('account_basic_information_next_step_button'),
@@ -206,12 +283,17 @@ class BasicInformationForm extends StatelessWidget {
               borderRadius: 30,
               disable: !state.enableNextButton(),
               onClick: () {
-                context
-                    .read<AccountBloc>()
-                    .add(const AccountCurrentStepChanged('next'));
-                controller.nextPage(
-                    duration: const Duration(milliseconds: 200),
-                    curve: Curves.ease);
+                if (state.isHongKongPermanentResident != null &&
+                    !state.isHongKongPermanentResident!) {
+                  NotEligibleScreen.openReplace(context);
+                } else {
+                  context
+                      .read<AccountBloc>()
+                      .add(const AccountCurrentStepChanged('next'));
+                  controller.nextPage(
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.ease);
+                }
               },
             );
           },
