4ef981fd66396948afb36381ba28d540538eb179
wahyu-loratech <wahyu@loratechai.com>
2022-07-25 13:56:40 +0700

    AL1-797 : What is FPS screen - Slicing UI - Screen test

---
+++ lib/feature/deposit/presentation/what_is_fps_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/presentation/custom_text.dart';
+
+class WhatIsFpsScreen extends StatelessWidget {
+  const WhatIsFpsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              CustomText(
+                'What is FPS?',
+                padding: EdgeInsets.only(bottom: 40),
+                type: FontType.h2,
+              ),
+              CustomText(
+                'FPS is a payment service platform that allows customers to make instant domestic payments to individuals and merchants in Hong Kong dollar or Renminbi via our online and mobile banking. It will be available 24/7, free of charge and covering participating local banks and financial institutes with Stored Value Facility (SVF) licenses.',
+                padding: EdgeInsets.only(bottom: 20),
+                type: FontType.bodyTextBold,
+              ),
+              CustomText(
+                'Traditional fund transfers require the payee’s bank account number, but FPS supports payments made using the payee’s mobile number, email address, FPS Identifier or HKID. FPS also allows registered payees to use a QR code to receive payments (a Mobile Security Key or Security Device might be required to complete the transaction). You can also receive payments and transfers from institutions, corporates and the HKSAR government via your Hong Kong ID number.',
+                type: FontType.bodyTextBold,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  static void open(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => const WhatIsFpsScreen()));
+  }
+}
