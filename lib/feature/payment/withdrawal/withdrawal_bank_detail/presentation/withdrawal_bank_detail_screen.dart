955eda97bdf102c720cc8e57c1443efe6f6649bc
Surya Oktana <surya.oktana@loratechai.com>
2022-07-26 16:41:13 +0700

    refactor some codes

--- lib/feature/payment/withdrawal/withdrawal_bank_detail/presentation/withdrawal_bank_detail_screen.dart
+++ lib/feature/payment/withdrawal/withdrawal_bank_detail/presentation/withdrawal_bank_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../../../core/presentation/custom_text.dart';
 import '../../bloc/withdrawal_bloc.dart';
+import '../../presentation/custom_withdrawal_text_information_widget.dart';
 import '../../presentation/custom_withdrawal_widget.dart';
 import '../../presentation/withdrawal_next_button.dart';
 
@@ -9,65 +10,39 @@ class WithdrawalBankDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomWithdrawalWidget(
+    return const CustomWithdrawalWidget(
       title: 'Your Bank Details',
       backTo: WithdrawalPages.welcome,
-      navigationButton: const WithdrawalNextButton(
+      navigationButton: WithdrawalNextButton(
           key: Key('withdrawal_bank_detail_screen_next_button'),
           nextTo: WithdrawalPages.amount,
           disable: false),
       children: [
-        const CustomText(
+        CustomText(
           'Please confirm your bank details. Your withdrawal will be credited to this account',
           type: FontType.smallTextBold,
           textAlign: TextAlign.justify,
         ),
-        const SizedBox(
+        SizedBox(
           height: 20,
         ),
-        const CustomText(
+        CustomText(
           'If your bank details are different from below, please contact our customer support for further assistance',
           type: FontType.smallTextBold,
           textAlign: TextAlign.justify,
         ),
-        const SizedBox(
+        SizedBox(
           height: 20,
         ),
-        _customContainer('Account No.', '1234567890'),
-        _customContainer('Bank Name',
-            'THE HONG KONG AND SHANGHAI BANKING CORPORATION LIMITED'),
-        _customContainer('Bank No.', '813'),
-        _customContainer('Account Name', 'LEUNG CHIU WAI')
+        CustomWithdrawalTextInformationWidget(
+            title: 'Account No.', label: '1234567890'),
+        CustomWithdrawalTextInformationWidget(
+            title: 'Bank Name',
+            label: 'THE HONG KONG AND SHANGHAI BANKING CORPORATION LIMITED'),
+        CustomWithdrawalTextInformationWidget(title: 'Bank No.', label: '813'),
+        CustomWithdrawalTextInformationWidget(
+            title: 'Account Name', label: 'LEUNG CHIU WAI')
       ],
     );
   }
-
-  Widget _customContainer(String title, String label) => Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomText(
-              title,
-              type: FontType.smallTextBold,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(4)),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
-                child: CustomText(
-                  label,
-                  type: FontType.bodyText,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
 }
