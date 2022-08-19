20aa0ac04dbee6586bb9db677286218f9a230b51
Krishna Gupta <krishna.gupta@loratechai.com>
2022-08-19 12:49:24 +0800

    Added dotenv in order to read the environments variables

--- increment_version.sh
+++ increment_version.sh
@@ -6,13 +6,9 @@ do
     eval ${key}=\${value}
 done < "$file"
 
-VERSION=$(git show-ref --head --hash head --abbrev=5)
-VERSION_NAME=${major}'.'${minor}'.'${patch}'.'$VERSION
+VERSION_NAME=${major}'.'${minor}'.'${patch}
 
 VERSION_CODE=$(( $major * 10000 + $minor * 100 + $patch))
-echo $VERSION
-echo $VERSION_CODE
-echo $VERSION_NAME
 
 sed -i "" "/flutter.versionName=/ s/=.*/=$VERSION_NAME/" android/local.properties
 sed -i "" "/flutter.versionCode=/ s/=.*/=$VERSION_CODE/" android/local.properties
@@ -23,11 +19,9 @@ plutil -replace CFBundleVersion -string $VERSION_CODE ios/Runner/Info.plist
 
 NEW_PATCH=$(( $patch + 1 ))
 
-echo $NEW_PATCH
-
 sed -i "" "/patch=/ s/=.*/=$NEW_PATCH/" version.properties
 
 #TODO: Open this in CI/CD Pipeline
 #git add version.properties
-#git commit -m "increased version"
+#git commit -m "Updated the version.properties file"
 
