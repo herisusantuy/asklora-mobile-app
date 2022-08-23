48b745d697c207c1cd304cf821a38d2c2b0d8ffe
wahyu-loratech <wahyu@loratechai.com>
2022-08-23 17:35:35 +0700

    AL1-996 & AL-1017 : -  Layout UI Select Order Type

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,8 @@
+class AppIcons {
+  static String barChart = 'assets/icons/bar_chart.png';
+  static String stop = 'assets/icons/stop.png';
+  static String saveMoney = 'assets/icons/save_money.png';
+  static String route = 'assets/icons/route.png';
+  static String financial = 'assets/icons/financial.png';
+  // static String money = 'assets/icons/money.svg';
+}
---
+++ lib/feature/orders/presentations/order_type_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/presentation/custom_text.dart';
+import '../../../core/utils/app_icons.dart';
+
+enum OrderType { buy, sell, unknown }
+
+class OrderTypeScreen extends StatelessWidget {
+  final OrderType orderType;
+  const OrderTypeScreen({
+    Key? key,
+    required this.orderType,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+        child: ListView(
+          physics: const ScrollPhysics(),
+          children: [
+            const CustomText(
+              'Select Order type',
+              type: FontType.h2,
+              padding: EdgeInsets.only(bottom: 20),
+            ),
+            const CustomText(
+              'AskLORa supports the\nfollowing order type :',
+              padding: EdgeInsets.only(bottom: 20),
+            ),
+            _listMenus(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _listMenus() {
+    return Column(children: [
+      if (orderType == OrderType.buy) ...[
+        _customListMenu(
+            'Market Order', 'Buy the stock at the current market price',
+            icon: AppIcons.saveMoney, onPress: () {}),
+        _customListMenu(
+            'Limit Order', 'Buy the stock at a specified limit price or lower',
+            icon: AppIcons.barChart, onPress: () {}),
+        _customListMenu('Stop Limit Order',
+            'Buy the stock if it rises to a specified stop price',
+            icon: AppIcons.stop, onPress: () {}),
+        _customListMenu('Stop Limit Order',
+            'Buy a limit order if the stock rises to a specified stop price',
+            icon: AppIcons.financial, onPress: () {}),
+        _customListMenu('Trailing Stop Order',
+            "Trail the stock's price and buy if it goes above the trail limit you've set",
+            icon: AppIcons.route, onPress: () {}),
+        const CustomText(
+          'Still unsure about the different order types? Learn more here',
+          textAlign: TextAlign.center,
+          padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+        ),
+      ] else ...[
+        _customListMenu(
+            'Market Order', 'Sell the stock at the current market price',
+            icon: AppIcons.saveMoney, onPress: () {}),
+        _customListMenu('Limit Order',
+            'Sell the stock at a specified limit price or higher',
+            icon: AppIcons.barChart, onPress: () {}),
+        _customListMenu('Stop Limit Order',
+            'Sell the stock if it falls to a specified stop price',
+            icon: AppIcons.stop, onPress: () {}),
+        _customListMenu('Stop Limit Order',
+            'Sell a limit order if the stock falls to a specified stop price',
+            icon: AppIcons.financial, onPress: () {}),
+        _customListMenu('Trailing Stop Order',
+            'Trail the stock’s price and sell if it goes below the trail limit you’ve set',
+            icon: AppIcons.route, onPress: () {}),
+      ]
+    ]);
+  }
+
+  Widget _customListMenu(
+    String title,
+    String text, {
+    String icon = '',
+    Function()? onPress,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        children: [
+          Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    title,
+                    type: FontType.h5,
+                    padding: const EdgeInsets.only(bottom: 5),
+                  ),
+                  CustomText(text),
+                ],
+              )),
+          Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: onPress,
+                    child: Image.asset(
+                      icon,
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
