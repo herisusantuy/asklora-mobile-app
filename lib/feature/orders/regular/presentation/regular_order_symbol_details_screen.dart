4c6068f5fb62c7468a703ea528a68a516f0acd1d
wahyu-loratech <wahyu@loratechai.com>
2022-08-25 13:20:32 +0700

    add safe area on symbol details screen

--- lib/feature/orders/regular/presentation/regular_order_symbol_details_screen.dart
+++ lib/feature/orders/regular/presentation/regular_order_symbol_details_screen.dart
@@ -8,41 +8,45 @@ class RegularOrderSymbolDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Column(
-          children: [
-            const SymbolDetailsWidget(),
-            Expanded(
-              //TODO: Add others widgets like graph and financials here.
-              child: Container(),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomTextButton(buttonText: 'BUY', onClick: () => {}),
-                      ],
-                    )),
-                const SizedBox(width: 10),
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        CustomTextButton(buttonText: 'SELL', onClick: () => {}),
-                      ],
-                    ))
-              ],
-            )
-          ],
-        ));
+    return SafeArea(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Column(
+            children: [
+              const SymbolDetailsWidget(),
+              Expanded(
+                //TODO: Add others widgets like graph and financials here.
+                child: Container(),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomTextButton(
+                              buttonText: 'BUY', onClick: () => {}),
+                        ],
+                      )),
+                  const SizedBox(width: 10),
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          CustomTextButton(
+                              buttonText: 'SELL', onClick: () => {}),
+                        ],
+                      ))
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
