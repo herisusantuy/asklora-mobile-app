7d2650108cb7c61f45182f917b2fb70d90b584d5
wahyu-loratech <wahyu@loratechai.com>
2022-09-14 11:44:00 +0700

    use BlocBuilder on stop order screen

--- lib/feature/orders/regular/presentation/stop_order_widget.dart
+++ lib/feature/orders/regular/presentation/stop_order_widget.dart
@@ -1,40 +1,102 @@
 part of 'order_screen.dart';
 
 class StopOrderWidget extends StatelessWidget {
-  final OrderType orderType;
+  final OrderState orderState;
   final SymbolDetail symbolDetail;
-  final TransactionType transactionType;
 
   const StopOrderWidget(
-      {required this.orderType,
-      required this.transactionType,
-      required this.symbolDetail,
-      Key? key})
+      {required this.orderState, required this.symbolDetail, Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: [
-        OrderTypePriceWidget(
-          prefixTitle: orderType.name,
-          value: '110',
-          onChanged: (_) {},
+        Expanded(
+          child: ListView(
+            physics: const ScrollPhysics(),
+            children: [
+              OrderTypePriceWidget.input(
+                prefixTitle: orderState.orderType.name,
+                onChanged: (value) => context.read<StopBloc>().add(
+                    StopPriceChanged(
+                        value.isNotEmpty ? double.parse(value) : 0)),
+              ),
+              SharesQuantityWidget.input(
+                onChanged: (value) => context.read<StopBloc>().add(
+                    StopQuantityChanged(
+                        value.isNotEmpty ? double.parse(value) : 0)),
+              ),
+              const TimeInForceWidget(),
+              const TradingHoursWidget(),
+              const SizedBox(
+                height: 40,
+              ),
+              MarketPriceWidget(symbolDetail: symbolDetail),
+              BlocBuilder<StopBloc, StopState>(
+                  buildWhen: (previous, current) =>
+                      previous.estimateTotal != current.estimateTotal,
+                  builder: (context, state) => EstimatedTotalWidget(
+                      value: state.estimateTotal.toString())),
+              if (orderState.transactionType == TransactionType.buy) ...[
+                BlocBuilder<StopBloc, StopState>(
+                  buildWhen: (previous, current) =>
+                      previous.availableBuyingPower !=
+                      current.availableBuyingPower,
+                  builder: (context, state) => AvailableBuyingPowerWidget(
+                      value: state.availableBuyingPower.toString()),
+                ),
+              ] else if (orderState.transactionType ==
+                  TransactionType.sell) ...[
+                BlocBuilder<StopBloc, StopState>(
+                  buildWhen: (previous, current) =>
+                      previous.availableAmountToSell !=
+                      current.availableAmountToSell,
+                  builder: (context, state) => AvailableAmountToSellWidget(
+                      value: state.availableAmountToSell.toString()),
+                ),
+                BlocBuilder<StopBloc, StopState>(
+                  buildWhen: (previous, current) =>
+                      previous.numberOfSellableShares !=
+                      current.numberOfSellableShares,
+                  builder: (context, state) => NumberOfSellableSharesWidget(
+                      value: state.numberOfSellableShares.toString()),
+                ),
+              ],
+            ],
+          ),
         ),
-        const SharesQuantityWidget(value: '4'),
-        const TimeInForceWidget(),
-        const TradingHoursWidget(),
-        const SizedBox(
-          height: 40,
-        ),
-        MarketPriceWidget(symbolDetail: symbolDetail),
-        const EstimatedTotalWidget(value: r'$320'),
-        if (transactionType == TransactionType.buy) ...[
-          const AvailableBuyingPowerWidget(value: r'$10,000'),
-        ] else if (transactionType == TransactionType.sell) ...[
-          const AvailableAmountToSellWidget(value: r'$10,000'),
-          const NumberOfSellableSharesWidget(value: '10'),
-        ],
+        BlocBuilder<StopBloc, StopState>(
+          buildWhen: (previous, current) =>
+              orderState.transactionType == TransactionType.buy &&
+                  previous.buyErrorText != current.buyErrorText ||
+              orderState.transactionType == TransactionType.sell &&
+                  previous.sellErrorText != current.sellErrorText ||
+              previous.response.state != current.response.state ||
+              previous.stopPrice != current.stopPrice ||
+              previous.quantity != current.quantity,
+          builder: (context, state) => OrderConfirmationButton<StopState>(
+            dynamicState: state,
+            errorText: orderState.transactionType == TransactionType.buy
+                ? state.buyErrorText
+                : state.sellErrorText,
+            isLoading: state.response.state == ResponseState.loading,
+            disable: state.buyErrorText.isNotEmpty ||
+                    state.stopPrice == 0 ||
+                    state.quantity == 0
+                ? true
+                : false,
+            orderState: context.read<OrderBloc>().state,
+            symbolDetail: symbolDetail,
+            onConfirmedTap: () => context.read<StopBloc>().add(
+                StopOrderSubmitted(OrderRequest.limit(
+                    symbolType: symbolDetail.symbolType.name,
+                    symbol: symbolDetail.name,
+                    side: orderState.transactionType.name,
+                    quantity: state.quantity.toString(),
+                    limitPrice: state.stopPrice.toString()))),
+          ),
+        )
       ],
     );
   }
