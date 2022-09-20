d144126cfffe1226c55a190bdfaf2b0bcafa959f
wahyu-loratech <wahyu@loratechai.com>
2022-09-20 15:46:35 +0700

    add controller on share quantity widget

--- lib/feature/orders/regular/presentation/widgets/shares_quantity_widget.dart
+++ lib/feature/orders/regular/presentation/widgets/shares_quantity_widget.dart
@@ -4,18 +4,23 @@ class SharesQuantityWidget extends StatelessWidget {
   final String value;
   final Function(String)? onChanged;
   final bool showOnlyInformation;
+  final TextEditingController? controller;
 
   const SharesQuantityWidget(
       {this.showOnlyInformation = false,
       this.onChanged,
       this.value = '',
+      this.controller,
       Key key = const Key('shares_quantity_widget')})
       : super(key: key);
 
-  static SharesQuantityWidget input({required Function(String) onChanged}) {
+  static SharesQuantityWidget input(
+      {required Function(String) onChanged,
+      TextEditingController? controller}) {
     return SharesQuantityWidget(
       onChanged: onChanged,
       showOnlyInformation: false,
+      controller: controller,
     );
   }
 
@@ -34,11 +39,13 @@ class SharesQuantityWidget extends StatelessWidget {
       child: showOnlyInformation
           ? null
           : CustomTextInput(
+              controller: controller,
               textInputType: TextInputType.number,
               textInputFormatterList: [
                 FilteringTextInputFormatter.digitsOnly,
               ],
               labelText: 'Quantity',
+              hintText: '0',
               onChanged: onChanged ?? (_) {},
             ),
     );
