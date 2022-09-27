12fc0642f0ea39c714186238f0631bd316be77a6
wahyu-loratech <wahyu@loratechai.com>
2022-09-27 13:27:29 +0700

    add confirm button

--- lib/feature/questions/presentation/personalisation_question/personalisation_question_screen.dart
+++ lib/feature/questions/presentation/personalisation_question/personalisation_question_screen.dart
@@ -6,9 +6,7 @@ import '../../../../core/presentation/navigation/custom_navigation_widget.dart';
 import '../../bloc/question/question_bloc.dart';
 import '../../domain/fixture.dart';
 import '../../domain/question.dart';
-import '../widget/descriptive_question_widget.dart';
-import '../widget/multiple_question_widget/multiple_question_widget.dart';
-import '../widget/slider_question_widget.dart';
+import '../widget/slider_question_widget/slider_question_widget.dart';
 import 'bloc/personalisation_question_bloc.dart';
 
 class PersonalisationQuestionScreen extends StatelessWidget {
@@ -23,14 +21,15 @@ class PersonalisationQuestionScreen extends StatelessWidget {
     return CustomNavigationWidget(
       header: const SizedBox(),
       child: BlocProvider(
-        create: (_) => PersonalizationQuestionBloc(initialIndex: initialIndex)
+        create: (_) => PersonalisationQuestionBloc(initialIndex: initialIndex)
           ..add(NextPersonalisationQuestion()),
-        child: BlocConsumer<PersonalizationQuestionBloc,
+        child: BlocConsumer<PersonalisationQuestionBloc,
             PersonalisationQuestionState>(
           listener: (context, state) {
             if (state is OnNextPersonalizationQuestion) {
-              context.read<QuestionBloc>().add(PrivacyQuestionIndexChanged(
-                  state.personalizationQuestionIndex));
+              context.read<QuestionBloc>().add(
+                  PersonalisationQuestionIndexChanged(
+                      state.personalizationQuestionIndex));
             } else if (state is OnNextToInvestmentStyleQuestionScreen) {
               context
                   .read<NavigationBloc<QuestionPageStep>>()
@@ -46,22 +45,16 @@ class PersonalisationQuestionScreen extends StatelessWidget {
               QuestionCollection questionCollection = state.question;
               switch (state.questionType) {
                 case QuestionType.choices:
-                  return MultipleChoiceQuestionWidget(
+                  return SliderQuestionWidget(
                     key: Key(questionCollection.uid!),
                     questionCollection: questionCollection,
                     defaultChoiceIndex: -1,
                   );
-                case (QuestionType.descriptive):
-                  return DescriptiveQuestionWidget(
-                      questionCollection: questionCollection);
-                case (QuestionType.slider):
-                  return SliderQuestionWidget(
-                      questionCollection: questionCollection);
                 default:
                   return const SizedBox();
               }
             } else {
-              return const SizedBox.shrink();
+              return const SizedBox();
             }
           },
         ),
