a4e59d1497d167d44da33522f78e27a97d30d020
Surya Oktana <surya.oktana@loratechai.com>
2022-11-02 15:49:58 +0700

    add generated files

---
+++ lib/feature/payment/deposits/shareable/proof_of_remittance/domain/proof_of_remittance_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'proof_of_remittance_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProofOfRemittanceResponse _$ProofOfRemittanceResponseFromJson(
+        Map<String, dynamic> json) =>
+    ProofOfRemittanceResponse(
+      json['detail'] as String,
+    );
+
+Map<String, dynamic> _$ProofOfRemittanceResponseToJson(
+        ProofOfRemittanceResponse instance) =>
+    <String, dynamic>{
+      'detail': instance.detail,
+    };
