3ef22eedd808484a5584508d297d76d0d2fd9f2e
Surya Oktana <surya.oktana@loratechai.com>
2023-01-10 16:16:38 +0700

    add example code

--- lib/main_test.dart
+++ lib/main_test.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
+import 'core/presentation/bot_badge/lora_pop_up_message_with_bot_badge.dart';
 import 'core/presentation/buttons/button_example.dart';
 import 'core/presentation/text_fields/text_field_example.dart';
-import 'core/values/app_values.dart';
+import 'core/styles/asklora_colors.dart';
+import 'feature/bot_stock/utils/bot_stock_utils.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) => MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Asklora Testing',
       home: Scaffold(
-        body: Padding(
-          padding: AppValues.screenHorizontalPadding,
-          child: ListView(
-            padding: const EdgeInsets.symmetric(vertical: 30),
-            children: const [
-              ButtonExample(),
-              SizedBox(
-                height: 24,
-              ),
-              TextFieldExample()
-            ],
-          ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          children: const [
+            LoraPopUpMessageWithBotBadge(
+              backgroundColor: AskLoraColors.primaryGreen,
+              withLoraImage: true,
+              badgePosition: BadgePosition.belowSubtitle,
+              title: 'No traded BotStocks.',
+              subTitle:
+                  'You can manage all your investments here after you start trading. Create an account and start trading.',
+              botTypes: [BotType.pullUp, BotType.squat],
+            ),
+            ButtonExample(),
+            SizedBox(
+              height: 24,
+            ),
+            TextFieldExample()
+          ],
         ),
       ));
 }
