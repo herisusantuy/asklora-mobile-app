b0407f4f85915ddb35f3eee32cd3e5471e0b2489
wahyuarhel <akunwahyuarhel@gmail.com>
2023-02-03 10:28:55 +0700

    function to check user's condition when sign in

--- lib/feature/auth/sign_in/domain/sign_in_response.g.dart
+++ lib/feature/auth/sign_in/domain/sign_in_response.g.dart
@@ -10,10 +10,12 @@ SignInResponse _$SignInResponseFromJson(Map<String, dynamic> json) =>
     SignInResponse(
       json['access'] as String,
       json['refresh'] as String,
+      userJourney: json['userJourney'] as String? ?? 'kyc',
     );
 
 Map<String, dynamic> _$SignInResponseToJson(SignInResponse instance) =>
     <String, dynamic>{
       'access': instance.access,
       'refresh': instance.refresh,
+      'userJourney': instance.userJourney,
     };
