136c536f4bbe930379c2a4552ba10bc090a52239
askloraheri <heri.susanto@loratechai.com>
2023-02-08 10:52:53 +0700

    Update disclosure_summary_content.dart

--- lib/feature/onboarding/kyc/presentation/financial_profile/widgets/disclosure_summary_content.dart
+++ lib/feature/onboarding/kyc/presentation/financial_profile/widgets/disclosure_summary_content.dart
@@ -38,9 +38,7 @@ class DisclosureSummaryContent extends StatelessWidget {
           _spaceHeight,
           SummaryTextInfo(
               title: 'Nature of Business',
-              subTitle: financialProfileState.natureOfBusiness != null
-                  ? financialProfileState.natureOfBusiness!.value
-                  : ''),
+              subTitle: financialProfileState.natureOfBusiness?.value ?? ''),
           _spaceHeight,
           SummaryTextInfo(
               title: 'Occupation',
