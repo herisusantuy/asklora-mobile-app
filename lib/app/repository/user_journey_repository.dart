d56541bdc016a3b91f256fa2d5e6674d221b7a0a
askloraheri <heri.susanto@loratechai.com>
2023-02-14 11:44:29 +0700

    Add try catch on save user journey and modify the way get user journey

--- lib/app/repository/user_journey_repository.dart
+++ lib/app/repository/user_journey_repository.dart
@@ -14,19 +14,41 @@ class UserJourneyRepository {
 
   Future<BaseResponse<UserJourneyResponse>> saveUserJourney(
       {required UserJourney userJourney, String? data}) async {
-    var response = await _userJourneyApiClient
-        .save(UserJourneyRequest(userJourney: userJourney.value, data: data));
-    var userJourneyResponse = UserJourneyResponse.fromJson(response.data!);
-    return BaseResponse.complete(userJourneyResponse);
+    await _sharedPreference.writeData('user_journey', userJourney.value);
+    try {
+      var response = await _userJourneyApiClient
+          .save(UserJourneyRequest(userJourney: userJourney.value, data: data));
+      var userJourneyResponse = UserJourneyResponse.fromJson(response.data!);
+      return BaseResponse.complete(userJourneyResponse);
+    } catch (e) {
+      ///TODO POST TO FIREBASE
+      return BaseResponse.error('Failed save user journey!');
+    }
   }
 
   Future<UserJourney> getUserJourney() async {
     try {
+      String? localUserJourneyString =
+          await _sharedPreference.readData('user_journey');
+      UserJourney localUserJourney = UserJourney.values
+          .firstWhere((element) => element.value == localUserJourneyString);
+
       var response = await _userJourneyApiClient.get();
       var userJourneyResponse = UserJourneyResponse.fromJson(response.data);
-      return UserJourney.values.firstWhereOrNull(
-              (element) => element.value == userJourneyResponse.userJourney) ??
-          UserJourney.privacy;
+
+      var indexUserJourneyResponse = UserJourney.values.indexWhere(
+          (element) => element.value == userJourneyResponse.userJourney);
+      var indexUserJourneyLocal = UserJourney.values
+          .indexWhere((element) => element.value == localUserJourney.value);
+
+      if (indexUserJourneyResponse < indexUserJourneyLocal) {
+        saveUserJourney(userJourney: localUserJourney);
+        return localUserJourney;
+      } else {
+        return UserJourney.values.firstWhereOrNull((element) =>
+                element.value == userJourneyResponse.userJourney) ??
+            UserJourney.privacy;
+      }
     } catch (e) {
       return UserJourney.privacy;
     }
