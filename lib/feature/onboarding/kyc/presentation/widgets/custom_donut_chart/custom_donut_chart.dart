719e2702dd2544eab5adff7ff98ddfa617ec2a6f
wahyuarhel <akunwahyuarhel@gmail.com>
2023-02-14 09:53:52 +0700

    init custom donut chart

---
+++ lib/feature/onboarding/kyc/presentation/widgets/custom_donut_chart/custom_donut_chart.dart
@@ -0,0 +1,52 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/presentation/custom_text_new.dart';
+import '../../../../../../core/styles/asklora_colors.dart';
+
+class CustomDonutChart extends StatelessWidget {
+  final int total;
+  final List<PieChartSectionData>? sections;
+  const CustomDonutChart({
+    Key? key,
+    this.total = 0,
+    this.sections,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 16 / 8,
+      child: Stack(
+        children: [
+          PieChart(
+            PieChartData(
+              sectionsSpace: 0,
+              centerSpaceRadius: 40,
+              startDegreeOffset: -90,
+              sections: sections,
+              borderData: FlBorderData(
+                show: true,
+                border: Border.all(
+                  color: AskLoraColors.black,
+                  strokeAlign: 20,
+                  width: 2,
+                ),
+              ),
+            ),
+            // swapAnimationCurve: Curves.easeInQuad,
+          ),
+          Align(
+              alignment: Alignment.center,
+              child: CustomTextNew(
+                '$total %',
+                style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    fontFamilyFallback: ['Mulish']),
+              )),
+        ],
+      ),
+    );
+  }
+}
