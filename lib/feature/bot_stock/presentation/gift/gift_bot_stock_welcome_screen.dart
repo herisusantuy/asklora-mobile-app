62daeb42c03b75326cb0d14084dd4f109ebe31fa
wahyuarhel <akunwahyuarhel@gmail.com>
2023-02-16 14:42:07 +0700

    change text content in gift bot stock welcome screen

--- lib/feature/bot_stock/presentation/gift/gift_bot_stock_welcome_screen.dart
+++ lib/feature/bot_stock/presentation/gift/gift_bot_stock_welcome_screen.dart
@@ -25,7 +25,7 @@ class GiftBotStockWelcomeScreen extends StatelessWidget {
                 text: 'Select a FREE AI trade now!'),
           ),
           CustomTextNew(
-            'Select a FREE Botstock worths USD\$68 based on your investment style that you set before.',
+            'Ready? Select a FREE Botstock\nworths HKD500 based on your\n investment style that you set\n before.\n\nGo! Move Your A%\$',
             style: AskLoraTextStyles.body1.copyWith(color: AskLoraColors.white),
             textAlign: TextAlign.center,
           )
