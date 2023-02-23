fa4ba5235158de6177ede42745f2d746c67b55e5
askloraheri <heri.susanto@loratechai.com>
2023-02-23 09:57:38 +0700

    Resolve forgot password screen test issue

--- test/forgot_password/forgot_password_screen_test.dart
+++ test/forgot_password/forgot_password_screen_test.dart
@@ -1,7 +1,7 @@
+import 'package:asklora_mobile_app/core/presentation/buttons/primary_button.dart';
 import 'package:asklora_mobile_app/feature/auth/forgot_password/presentation/forgot_password_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:asklora_mobile_app/core/presentation/custom_text_button.dart';
 
 import '../mocks/mocks.dart';
 
@@ -9,7 +9,7 @@ void main() {
   group(
     'Forgot Password Screen Widget Test',
     () {
-      Future<void> _buildForgotPasswordScreen(WidgetTester tester) async {
+      Future<void> buildForgotPasswordScreen(WidgetTester tester) async {
         final mockObserver = MockNavigatorObserver();
         await tester.pumpWidget(
           MaterialApp(
@@ -23,7 +23,7 @@ void main() {
         'Show Forgot Password Screen with `Email` input field with `Submit` button',
         (tester) async {
           //ARRANGE
-          await _buildForgotPasswordScreen(tester);
+          await buildForgotPasswordScreen(tester);
           // ACT & ASSERT
           var emailInput = find.byKey(
             const Key('forgot_password_email_input'),
@@ -39,23 +39,24 @@ void main() {
       testWidgets(
           'Render error label on email text field and button disable when entered wrong email.',
           (tester) async {
-        await _buildForgotPasswordScreen(tester);
+        await buildForgotPasswordScreen(tester);
         await tester.enterText(
           find.byKey(const Key('forgot_password_email_input')),
-          'wkwkwkwkwk',
+          'john.doe',
         );
         var submitButton = find.byKey(
           const Key('forgot_password_submit_button'),
         );
         await tester.pump();
 
-        expect(find.text('wkwkwkwkwk'), findsOneWidget);
+        expect(find.text('john.doe'), findsOneWidget);
         expect(find.text('Enter valid email'), findsOneWidget);
-        expect(tester.widget<CustomTextButton>(submitButton).disable, isTrue);
+        expect((tester.firstWidget(submitButton) as PrimaryButton).disabled,
+            isTrue);
       });
 
       testWidgets('Enable button when entered valid email.', (tester) async {
-        await _buildForgotPasswordScreen(tester);
+        await buildForgotPasswordScreen(tester);
         await tester.enterText(
           find.byKey(const Key('forgot_password_email_input')),
           'asklora@loratechai.com',
@@ -67,7 +68,8 @@ void main() {
 
         expect(find.text('asklora@loratechai.com'), findsOneWidget);
         expect(find.text('Enter valid email'), findsNothing);
-        expect(tester.widget<CustomTextButton>(submitButton).disable, isFalse);
+        expect((tester.firstWidget(submitButton) as PrimaryButton).disabled,
+            isFalse);
       });
     },
   );
