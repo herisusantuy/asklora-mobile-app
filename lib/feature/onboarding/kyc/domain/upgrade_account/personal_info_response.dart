bff2e6d159f819f2a7155ad2cf99289409b8df0d
wahyuarhel <akunwahyuarhel@gmail.com>
2023-02-27 14:32:37 +0700

    update personal info response

--- lib/feature/onboarding/kyc/domain/upgrade_account/personal_info_response.dart
+++ lib/feature/onboarding/kyc/domain/upgrade_account/personal_info_response.dart
@@ -6,17 +6,9 @@ part 'personal_info_response.g.dart';
 @JsonSerializable()
 class PersonalInfoResponse extends Equatable {
   final String? detail;
-  @JsonKey(name: 'postal_code')
-  final String? postalCode;
-  final String? city;
-  @JsonKey(name: 'address_line_2')
-  final String? addressLine2;
 
   const PersonalInfoResponse({
     this.detail,
-    this.postalCode,
-    this.city,
-    this.addressLine2,
   });
 
   factory PersonalInfoResponse.fromJson(Map<String, dynamic> json) =>
@@ -25,10 +17,5 @@ class PersonalInfoResponse extends Equatable {
   Map<String, dynamic> toJson() => _$PersonalInfoResponseToJson(this);
 
   @override
-  List<Object?> get props => [
-        detail ?? '',
-        postalCode ?? '',
-        city ?? '',
-        addressLine2 ?? '',
-      ];
+  List<Object?> get props => [detail ?? ''];
 }
--- lib/feature/onboarding/kyc/domain/upgrade_account/personal_info_response.g.dart
+++ lib/feature/onboarding/kyc/domain/upgrade_account/personal_info_response.g.dart
@@ -10,16 +10,10 @@ PersonalInfoResponse _$PersonalInfoResponseFromJson(
         Map<String, dynamic> json) =>
     PersonalInfoResponse(
       detail: json['detail'] as String?,
-      postalCode: json['postal_code'] as String?,
-      city: json['city'] as String?,
-      addressLine2: json['address_line_2'] as String?,
     );
 
 Map<String, dynamic> _$PersonalInfoResponseToJson(
         PersonalInfoResponse instance) =>
     <String, dynamic>{
       'detail': instance.detail,
-      'postal_code': instance.postalCode,
-      'city': instance.city,
-      'address_line_2': instance.addressLine2,
     };
