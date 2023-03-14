2aef6e2d971b9f98a2ccd526a21c07042747c726
wahyu.ramadhan <wahyu@loratechai.com>
2023-03-14 09:42:38 +0700

    revert code

--- lib/feature/onboarding/kyc/presentation/personal_info/pesonal_info_screen.dart
+++ lib/feature/onboarding/kyc/presentation/personal_info/pesonal_info_screen.dart
@@ -247,25 +247,22 @@ class PersonalInfoScreen extends StatelessWidget {
               }
             },
             child: ButtonPair(
-              // disablePrimaryButton: _disablePrimaryButton(state),
+              disablePrimaryButton: _disablePrimaryButton(state),
               primaryButtonOnClick: () {
-                // final state = context.read<PersonalInfoBloc>().state;
-                // context
-                //     .read<PersonalInfoBloc>()
-                //     .add(PersonalInfoSubmitted(PersonalInfoRequest(
-                //       firstName: state.firstName,
-                //       lastName: state.lastName,
-                //       gender: state.gender,
-                //       hkIdNumber: state.hkIdNumber,
-                //       nationality: state.nationalityCode,
-                //       dateOfBirth: state.dateOfBirth,
-                //       countryOfBirth: state.countryCodeOfBirth,
-                //       phoneCountryCode: state.phoneCountryCode,
-                //       phoneNumber: state.phoneNumber,
-                //     )));
+                final state = context.read<PersonalInfoBloc>().state;
                 context
-                    .read<NavigationBloc<KycPageStep>>()
-                    .add(const PageChanged(KycPageStep.signTaxAgreements));
+                    .read<PersonalInfoBloc>()
+                    .add(PersonalInfoSubmitted(PersonalInfoRequest(
+                      firstName: state.firstName,
+                      lastName: state.lastName,
+                      gender: state.gender,
+                      hkIdNumber: state.hkIdNumber,
+                      nationality: state.nationalityCode,
+                      dateOfBirth: state.dateOfBirth,
+                      countryOfBirth: state.countryCodeOfBirth,
+                      phoneCountryCode: state.phoneCountryCode,
+                      phoneNumber: state.phoneNumber,
+                    )));
               },
               secondaryButtonOnClick: () =>
                   TabsScreen.openAndRemoveAllRoute(context),
