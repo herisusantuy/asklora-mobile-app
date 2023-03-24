66aa86c9b09f5e7c8b2722bb43f0d2706c9de442
Surya Oktana <surya.oktana@loratechai.com>
2023-03-24 09:57:13 +0700

    bot orders auto generated files

---
+++ lib/feature/bot_stock/domain/orders/bot_create_order_request.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bot_create_order_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BotCreateOrderRequest _$BotCreateOrderRequestFromJson(
+        Map<String, dynamic> json) =>
+    BotCreateOrderRequest(
+      ticker: json['ticker'] as String,
+      botId: json['bot_id'] as String,
+      spotDate: json['spot_date'] as String,
+      investmentAmount: (json['investment_amount'] as num).toDouble(),
+      price: (json['price'] as num).toDouble(),
+      fraction: json['fraction'] as bool,
+      margin: json['margin'] as int,
+      orderType: json['order_type'] as String,
+      isAggregate: json['is_aggregate'] as bool,
+    );
+
+Map<String, dynamic> _$BotCreateOrderRequestToJson(
+        BotCreateOrderRequest instance) =>
+    <String, dynamic>{
+      'ticker': instance.ticker,
+      'bot_id': instance.botId,
+      'spot_date': instance.spotDate,
+      'investment_amount': instance.investmentAmount,
+      'price': instance.price,
+      'fraction': instance.fraction,
+      'margin': instance.margin,
+      'order_type': instance.orderType,
+      'is_aggregate': instance.isAggregate,
+    };
---
+++ lib/feature/bot_stock/domain/orders/bot_order_request.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bot_order_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BotOrderRequest _$BotOrderRequestFromJson(Map<String, dynamic> json) =>
+    BotOrderRequest(
+      orderId: json['order_id'] as String,
+      orderType: json['order_type'] as String,
+    );
+
+Map<String, dynamic> _$BotOrderRequestToJson(BotOrderRequest instance) =>
+    <String, dynamic>{
+      'order_id': instance.orderId,
+      'order_type': instance.orderType,
+    };
