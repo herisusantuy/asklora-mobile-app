05acdd6e758f9db16f4fe55a9aa52a0ee982422d
wahyu.ramadhan <wahyu@loratechai.com>
2023-04-10 16:08:36 +0700

    update code

--- lib/feature/onboarding/kyc/domain/get_account/get_account_response.dart
+++ lib/feature/onboarding/kyc/domain/get_account/get_account_response.dart
@@ -2,6 +2,10 @@ import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 import '../../utils/kyc_dropdown_enum.dart';
+import '../upgrade_account/agreement.dart';
+import '../upgrade_account/employment_info.dart';
+import '../upgrade_account/personal_info_request.dart';
+import '../upgrade_account/residence_info_request.dart';
 
 part 'get_account_response.g.dart';
 
@@ -11,9 +15,9 @@ class GetAccountResponse extends Equatable {
   final String username;
   final String email;
   @JsonKey(name: 'personal_info')
-  final PersonalInfo? personalInfo;
+  final PersonalInfoRequest? personalInfo;
   @JsonKey(name: 'residence_info')
-  final ResidenceInfo? residenceInfo;
+  final ResidenceInfoRequest? residenceInfo;
   @JsonKey(name: 'employment_info')
   final EmploymentInfo? employmentInfo;
   final List<Agreement> agreements;
@@ -41,174 +45,6 @@ class GetAccountResponse extends Equatable {
 
   @override
   List<Object> get props => [];
-}
-
-@JsonSerializable()
-class PersonalInfo {
-  @JsonKey(name: 'first_name')
-  final String firstName;
-
-  @JsonKey(name: 'last_name')
-  final String lastName;
-  final String gender;
-  @JsonKey(name: 'hkid_number')
-  final String hkIdNumber;
-
-  final String nationality;
-  @JsonKey(name: 'date_of_birth')
-  final String dateOfBirth;
-
-  @JsonKey(name: 'country_of_birth')
-  final String countryOfBirth;
-
-  @JsonKey(name: 'phone_country_code')
-  final String phoneCountryCode;
-
-  @JsonKey(name: 'phone_number')
-  final String phoneNumber;
-
-  const PersonalInfo({
-    this.firstName = '',
-    this.lastName = '-',
-    this.gender = '-',
-    this.hkIdNumber = '-',
-    this.nationality = '-',
-    this.dateOfBirth = '-',
-    this.countryOfBirth = '-',
-    this.phoneCountryCode = '-',
-    this.phoneNumber = '-',
-  });
-
-  factory PersonalInfo.fromJson(Map<String, dynamic> json) =>
-      _$PersonalInfoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$PersonalInfoToJson(this);
-
-  @override
-  String toString() {
-    return 'PersonalInfo{firstName: $firstName, lastName: $lastName, gender: $gender, hkIdNumber: $hkIdNumber, nationality: $nationality, dateOfBirth: $dateOfBirth, countryOfBirth: $countryOfBirth, phoneCountryCode: $phoneCountryCode, phoneNumber: $phoneNumber}';
-  }
-}
-
-@JsonSerializable()
-class ResidenceInfo {
-  @JsonKey(name: 'postal_code')
-  final String? postalCode;
-  final String? city;
-  final String? country;
-  @JsonKey(name: 'address_line_1')
-  final String addressLine1;
-  @JsonKey(name: 'address_line_2')
-  final String? addressLine2;
-  final String district;
-  final String region;
-
-  ResidenceInfo({
-    this.postalCode,
-    this.city,
-    this.country,
-    this.addressLine1 = '',
-    this.addressLine2,
-    this.district = '',
-    this.region = '',
-  });
-
-  factory ResidenceInfo.fromJson(Map<String, dynamic> json) =>
-      _$ResidenceInfoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$ResidenceInfoToJson(this);
-
-  @override
-  String toString() {
-    return 'ResidenceInfo(postalCode: $postalCode, city: $city, country: $country, addressLine1: $addressLine1, addressLine2: $addressLine2, district: $district, region: $region)';
-  }
-}
-
-@JsonSerializable()
-class EmploymentInfo {
-  @JsonKey(name: 'postal_code')
-  final String? postalCode;
-  final String? city;
-  final String? country;
-  @JsonKey(name: 'employment_status')
-  final EmploymentStatus employmentStatus;
-  @JsonKey(name: 'employer_business')
-  final String? employerBusiness;
-  @JsonKey(name: 'employer_business_description')
-  final String? employerBusinessDescription;
-  final Occupations? occupation;
-  @JsonKey(name: 'employer_address_line_1')
-  final String? employerAddressLine1;
-  @JsonKey(name: 'employer_address_line_2')
-  final String? employerAddressLine2;
-  final String? district;
-  final String? region;
-  @JsonKey(name: 'different_country_reason')
-  final String? differentCountryReason;
-
-  EmploymentInfo({
-    this.postalCode,
-    this.city,
-    this.country,
-    this.employmentStatus = EmploymentStatus.unknown,
-    this.employerBusiness,
-    this.employerBusinessDescription,
-    this.occupation,
-    this.employerAddressLine1,
-    this.employerAddressLine2,
-    this.district,
-    this.region,
-    this.differentCountryReason,
-  });
-
-  factory EmploymentInfo.fromJson(Map<String, dynamic> json) =>
-      _$EmploymentInfoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$EmploymentInfoToJson(this);
-
-  @override
-  String toString() {
-    return 'EmploymentInfo(postalCode: $postalCode, city: $city, country: $country, employmentStatus: $employmentStatus, employerBusiness: $employerBusiness, employerBusinessDescription: $employerBusinessDescription, occupation: $occupation, employerAddressLine1: $employerAddressLine1, employerAddressLine2: $employerAddressLine2, district: $district, region: $region, differentCountryReason: $differentCountryReason)';
-  }
-}
-
-enum AgreementType {
-  ma('MA'),
-  aa('AA'),
-  ca('CA'),
-  aca('ACA'),
-  rds('RDS'),
-  w8ben('W8BEN');
-
-  final String value;
-  const AgreementType(this.value);
-}
-
-@JsonSerializable()
-class Agreement {
-  final AgreementType agreement;
-  @JsonKey(name: 'signed_at')
-  final String signedAt;
-  @JsonKey(name: 'ip_address')
-  final String? ipAddress;
-  final String? signature;
-
-  Agreement({
-    this.agreement = AgreementType.w8ben,
-    this.signedAt = '',
-    this.ipAddress,
-    this.signature,
-  });
-
-  factory Agreement.fromJson(Map<String, dynamic> json) =>
-      _$AgreementFromJson(json);
-
-  Map<String, dynamic> toJson() => _$AgreementToJson(this);
-
-  @override
-  String toString() {
-    return 'Agreement(agreement: $agreement, signedAt: $signedAt, ipAddress: $ipAddress, signature: $signature)';
-  }
 }
 
 @JsonSerializable()
