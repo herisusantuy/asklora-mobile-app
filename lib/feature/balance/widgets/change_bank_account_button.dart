64d8367107e731b26df6cf2d513b275817cd1996
Surya Oktana <surya.oktana@loratechai.com>
2023-05-15 16:39:21 +0700

    localisation

--- lib/feature/balance/widgets/change_bank_account_button.dart
+++ lib/feature/balance/widgets/change_bank_account_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../core/presentation/custom_text_new.dart';
 import '../../../core/styles/asklora_text_styles.dart';
+import '../../../generated/l10n.dart';
 import '../deposit/presentation/welcome/deposit_welcome_screen.dart';
 import '../deposit/utils/deposit_utils.dart';
 
@@ -9,18 +10,16 @@ class ChangeBankAccountButton extends StatelessWidget {
   const ChangeBankAccountButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => DepositWelcomeScreen.open(
-        context: context,
-        depositType: DepositType.firstTime,
-      ),
-      child: CustomTextNew(
-        'CHANGE BANK ACCOUNT',
-        style: AskLoraTextStyles.subtitle2.copyWith(
-          decoration: TextDecoration.underline,
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: () => DepositWelcomeScreen.open(
+          context: context,
+          depositType: DepositType.firstTime,
         ),
-      ),
-    );
-  }
+        child: CustomTextNew(
+          S.of(context).changeBankAccount,
+          style: AskLoraTextStyles.subtitle2.copyWith(
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      );
 }
