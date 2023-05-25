e01e2878dd294cc7c8eb348304db77929543b43c
Surya Oktana <surya.oktana@loratechai.com>
2023-05-25 09:20:10 +0700

    slightly change background position

--- lib/feature/onboarding/welcome/pages/carousel_page.dart
+++ lib/feature/onboarding/welcome/pages/carousel_page.dart
@@ -36,14 +36,11 @@ class CarouselPage extends StatelessWidget {
 
   List<Widget> get _backgroundImages => [
         Positioned(
-            right: 0,
-            child: Container(
-                transform: Matrix4.translationValues(0, -35, 0),
-                child: getPngImage('carousel_bg_dollar_3'))),
+            top: 30, right: 0, child: getPngImage('carousel_bg_dollar_3')),
         Positioned(
-            top: 170, left: 0, child: getPngImage('carousel_bg_dollar_1')),
+            top: 200, left: 0, child: getPngImage('carousel_bg_dollar_1')),
         Positioned(
-            top: 334, right: 1.61, child: getPngImage('carousel_bg_dollar_2')),
+            top: 364, right: 1.61, child: getPngImage('carousel_bg_dollar_2')),
       ];
 
   Widget _animatedTexts(BuildContext context) {
