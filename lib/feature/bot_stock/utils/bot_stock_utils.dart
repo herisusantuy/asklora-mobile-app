20e9b3a4d85e0c00a901e8ad720c57bde54c9e69
suryaoktana <surya@asklora.ai>
2023-06-06 21:28:32 +0700

    add bot status

--- lib/feature/bot_stock/utils/bot_stock_utils.dart
+++ lib/feature/bot_stock/utils/bot_stock_utils.dart
@@ -91,10 +91,18 @@ enum BotStockFilter {
 }
 
 enum BotStatus {
+  populated('initialized', 'Initialized', AskLoraColors.amber),
+  indicative('indicative', 'Indicative', AskLoraColors.amber),
+  live('initialized', 'Live', AskLoraColors.primaryGreen),
+  earlyTerminated(
+      'initialized', 'Early Terminated', AskLoraColors.primaryMagenta),
+  expired('initialized', 'Expired', AskLoraColors.primaryMagenta),
+  nearlyKnockOut('initialized', 'Nearly Knock Out', AskLoraColors.amber),
+  knockOut('initialized', 'Knock Out', AskLoraColors.primaryMagenta),
   pending('place', 'Pending', AskLoraColors.amber),
   active('open', 'Active', AskLoraColors.primaryGreen),
   activeExpireSoon('open', 'Active (Expire Soon)', AskLoraColors.primaryGreen),
-  closed('closed', 'Closed', AskLoraColors.primaryGreen),
+  closed('closed', 'Closed', AskLoraColors.primaryMagenta),
   cancel('cancel', 'Cancel', AskLoraColors.primaryMagenta);
 
   final String value;
