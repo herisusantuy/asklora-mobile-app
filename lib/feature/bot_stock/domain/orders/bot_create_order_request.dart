420f109d0af7c9c6ff9be871777baa92fd44967b
Krishna Gupta <krishna.gupta@loratechai.com>
2023-06-07 17:30:56 +0800

    rede's fix

--- lib/feature/bot_stock/domain/orders/bot_create_order_request.dart
+++ lib/feature/bot_stock/domain/orders/bot_create_order_request.dart
@@ -7,33 +7,33 @@ class BotCreateOrderRequest {
   final String ticker;
   @JsonKey(name: 'bot_id')
   final String botId;
-  @JsonKey(name: 'spot_date')
-  final String spotDate;
+  // @JsonKey(name: 'spot_date')
+  // final String spotDate;
   @JsonKey(name: 'investment_amount')
   final double investmentAmount;
-  final double price;
+  // final double price;
   final bool fraction;
   final int margin;
-  @JsonKey(name: 'order_type')
-  final String orderType;
+  // @JsonKey(name: 'order_type')
+  // final String orderType;
   @JsonKey(name: 'is_aggregate')
   final bool isAggregate;
-  @JsonKey(name: 'is_dummy')
-  final bool isDummy;
+  // @JsonKey(name: 'is_dummy')
+  // final bool isDummy;
 
   const BotCreateOrderRequest(
       {required this.ticker,
       required this.botId,
-      required this.spotDate,
+      // required this.spotDate,
       required this.investmentAmount,
-      required this.price,
+      // required this.price,
       this.fraction = true,
       this.margin = 1,
 
       ///TODO : REVERT BACK THE ORDERTYPE TO `POOL` LATER AFTER TESTING
-      this.orderType = 'POOL',
+      // this.orderType = 'POOL',
       this.isAggregate = false,
-      this.isDummy = false});
+      /*this.isDummy = false*/});
 
   factory BotCreateOrderRequest.fromJson(Map<String, dynamic> json) =>
       _$BotCreateOrderRequestFromJson(json);
--- lib/feature/bot_stock/domain/orders/bot_create_order_request.g.dart
+++ lib/feature/bot_stock/domain/orders/bot_create_order_request.g.dart
@@ -11,14 +11,10 @@ BotCreateOrderRequest _$BotCreateOrderRequestFromJson(
     BotCreateOrderRequest(
       ticker: json['ticker'] as String,
       botId: json['bot_id'] as String,
-      spotDate: json['spot_date'] as String,
       investmentAmount: (json['investment_amount'] as num).toDouble(),
-      price: (json['price'] as num).toDouble(),
       fraction: json['fraction'] as bool? ?? true,
       margin: json['margin'] as int? ?? 1,
-      orderType: json['order_type'] as String? ?? 'POOL',
       isAggregate: json['is_aggregate'] as bool? ?? false,
-      isDummy: json['is_dummy'] as bool? ?? false,
     );
 
 Map<String, dynamic> _$BotCreateOrderRequestToJson(
@@ -26,12 +22,8 @@ Map<String, dynamic> _$BotCreateOrderRequestToJson(
     <String, dynamic>{
       'ticker': instance.ticker,
       'bot_id': instance.botId,
-      'spot_date': instance.spotDate,
       'investment_amount': instance.investmentAmount,
-      'price': instance.price,
       'fraction': instance.fraction,
       'margin': instance.margin,
-      'order_type': instance.orderType,
       'is_aggregate': instance.isAggregate,
-      'is_dummy': instance.isDummy,
     };
