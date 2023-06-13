c1eb4ff32c278a93e78ef51f06c17a538c3c76c9
Krishna Gupta <krishna.gupta@loratechai.com>
2023-06-13 17:23:56 +0800

    Configured the minimum deposits using feature flag

--- lib/core/utils/feature_flags.dart
+++ lib/core/utils/feature_flags.dart
@@ -3,4 +3,7 @@ class FeatureFlags {
   static const bool byPassOtpOnSlack = true;
   static const bool enableSentry = true;
   static const bool byPassFreeBots = true;
+
+  /// Currently only being used for Deposits.
+  static const bool isProdTestEnabled = true;
 }
--- lib/feature/balance/deposit/utils/deposit_utils.dart
+++ lib/feature/balance/deposit/utils/deposit_utils.dart
@@ -1,7 +1,8 @@
 import '../../../../core/utils/extensions.dart';
+import '../../../../core/utils/feature_flags.dart';
 
 enum DepositType {
-  firstTime(minDeposit: 100),
+  firstTime(minDeposit: FeatureFlags.isProdTestEnabled ? 100 : 10000),
   type1(minDeposit: 0),
   type2(minDeposit: 0);
 
