7db27cace5d882d52a29c313697b9b9c02705844
suryaoktana <surya@asklora.ai>
2023-06-13 15:50:11 +0700

    min deposit for first time = 100 HKD some bug fixes

--- lib/feature/balance/deposit/utils/deposit_utils.dart
+++ lib/feature/balance/deposit/utils/deposit_utils.dart
@@ -1,9 +1,13 @@
+import '../../../../core/utils/extensions.dart';
+
 enum DepositType {
-  firstTime(minDeposit: 10000),
+  firstTime(minDeposit: 100),
   type1(minDeposit: 0),
   type2(minDeposit: 0);
 
   final double minDeposit;
+
+  String get minDepositString => minDeposit.convertToCurrencyDecimal();
 
   const DepositType({required this.minDeposit});
 }
