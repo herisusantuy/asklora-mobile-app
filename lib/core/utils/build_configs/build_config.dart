398eec9678adb4edfc54878fd9e28d0067e37438
suryaoktana <surya@asklora.ai>
2023-06-14 13:39:08 +0700

    clean code move packages to respective folder remove payment packages

--- lib/core/utils/build_configs/build_config.dart
+++ lib/core/utils/build_configs/build_config.dart
@@ -29,11 +29,11 @@ class Environment {
 }
 
 class DevConfig implements BaseConfig {
-  @override
-  String get ppiBaseUrl => 'https://ppi-dev.intra.asklora.ai/';
-
-  @override
-  String get askLoraApiBaseUrl => 'https://dev-apca.intra.asklora.ai/';
+  // @override
+  // String get ppiBaseUrl => 'https://ppi-dev.intra.asklora.ai/';
+  //
+  // @override
+  // String get askLoraApiBaseUrl => 'https://dev-apca.intra.asklora.ai/';
 
   @override
   String get streamServiceBaseUrl => 'wss://apca.services.asklora.ai/';
@@ -54,12 +54,12 @@ class DevConfig implements BaseConfig {
 // @override
 // String get askLoraApiBaseUrl => 'http://192.168.100.198:8002/';
   ///HENDIKAS LOCAL
-// @override
-// String get askLoraApiBaseUrl => 'http://192.168.100.194:8000/';
-//
-// @override
-// String get ppiBaseUrl => 'http://192.168.100.224:8005/';
-//
+@override
+String get askLoraApiBaseUrl => 'http://192.168.100.194:8000/';
+
+@override
+String get ppiBaseUrl => 'http://192.168.100.224:8005/';
+
 // @override
 // String get streamServiceBaseUrl => 'wss://apca.services.asklora.ai/';
 }
