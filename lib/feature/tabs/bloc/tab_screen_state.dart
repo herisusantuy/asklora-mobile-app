f3e366c4ec3212c3583e6717e2eb05ec847dcd6a
suryaoktana <surya@asklora.ai>
2023-06-22 13:51:38 +0700

    add back button handler on tabscreen

--- lib/feature/tabs/bloc/tab_screen_state.dart
+++ lib/feature/tabs/bloc/tab_screen_state.dart
@@ -2,28 +2,32 @@ part of 'tab_screen_bloc.dart';
 
 enum TabPage { home, forYou, portfolio }
 
+enum TabScreenBackState { none, openConfirmation, closeApp }
+
 class TabScreenState extends Equatable {
   final TabPage currentTabPage;
   final bool aiPageSelected;
+  final TabScreenBackState tabScreenBackState;
 
   const TabScreenState({
     required this.currentTabPage,
     this.aiPageSelected = false,
+    this.tabScreenBackState = TabScreenBackState.none,
   });
 
   @override
-  List<Object> get props => [
-        currentTabPage,
-        aiPageSelected,
-      ];
+  List<Object> get props =>
+      [currentTabPage, aiPageSelected, tabScreenBackState];
 
   TabScreenState copyWith({
     TabPage? currentTabPage,
     bool? aiPageSelected,
+    TabScreenBackState? tabScreenBackState,
   }) {
     return TabScreenState(
       currentTabPage: currentTabPage ?? this.currentTabPage,
       aiPageSelected: aiPageSelected ?? this.aiPageSelected,
+      tabScreenBackState: tabScreenBackState ?? this.tabScreenBackState,
     );
   }
 }
