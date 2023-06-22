52a75337bc9eca0ee3df6ed1d556b2f74d7b50e2
suryaoktana <surya@asklora.ai>
2023-06-22 10:20:40 +0700

    revert build config and endpoints

--- lib/core/utils/build_configs/build_config.dart
+++ lib/core/utils/build_configs/build_config.dart
@@ -30,10 +30,10 @@ class Environment {
 
 class DevConfig implements BaseConfig {
   @override
-  String get ppiBaseUrl => 'http://192.168.100.224:8005/';
+  String get ppiBaseUrl => 'https://ppi-dev.intra.asklora.ai/';
 
   @override
-  String get askLoraApiBaseUrl => 'http://192.168.100.224:8002/';
+  String get askLoraApiBaseUrl => 'https://dev-apca.intra.asklora.ai/';
 
   @override
   String get streamServiceBaseUrl => 'wss://apca.services.asklora.ai/';
