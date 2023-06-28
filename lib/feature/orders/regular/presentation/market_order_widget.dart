b54c992a4f8cf457f4da2712f4a23d09512d25e1
wahyu.ramadhan <wahyu@loratechai.com>
2023-06-28 21:37:28 +0700

    same like commit before

--- lib/feature/orders/regular/presentation/market_order_widget.dart
+++ lib/feature/orders/regular/presentation/market_order_widget.dart
@@ -141,10 +141,10 @@ class MarketOrderWidget extends StatelessWidget {
                     context.read<MarketBloc>().add(const ResetMarketValue());
                   },
                   style: ElevatedButton.styleFrom(
-                      primary: state.marketType == MarketType.amount
+                      backgroundColor: state.marketType == MarketType.amount
                           ? Colors.blue
                           : Colors.white,
-                      onPrimary: state.marketType == MarketType.amount
+                      foregroundColor: state.marketType == MarketType.amount
                           ? Colors.white
                           : Colors.blue,
                       shape: RoundedRectangleBorder(
