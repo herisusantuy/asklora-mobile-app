cd2eaba3d40bf93a41c737d3f85d8bfa961c43d8
suryaoktana <surya@asklora.ai>
2023-06-30 16:00:58 +0700

    hide terminate button on status waiting termination

--- lib/feature/bot_stock/presentation/portfolio/bloc/portfolio_state.dart
+++ lib/feature/bot_stock/presentation/portfolio/bloc/portfolio_state.dart
@@ -69,8 +69,11 @@ class PortfolioState extends Equatable {
   }
 
   bool get isShowTerminateButton =>
-      botActiveOrderDetailResponse.data?.botStatus == BotStatus.live ||
-      botActiveOrderDetailResponse.data?.botStatus == BotStatus.liveExpireSoon;
+      (botActiveOrderDetailResponse.data?.botStatus == BotStatus.live ||
+          botActiveOrderDetailResponse.data?.botStatus ==
+              BotStatus.liveExpireSoon) &&
+      botActiveOrderDetailResponse.data?.omsStatus !=
+          OmsStatus.waitingTermination;
 
   bool get isShowCancelButton =>
       botActiveOrderDetailResponse.data?.botStatus == BotStatus.pending;
