9ec8998c4cfbab995fe6511ebe504a611e0834dc
askloraheri <heri.susanto@loratechai.com>
2023-07-05 09:41:56 +0700

    Remove app bloc on custom silver persistent

--- lib/feature/tabs/home/custom_header/custom_sliver_persistent_header.dart
+++ lib/feature/tabs/home/custom_header/custom_sliver_persistent_header.dart
@@ -30,54 +30,47 @@ class CustomSliverPersistentHeader extends SliverPersistentHeaderDelegate {
             child: Container(
               color: AskLoraColors.charcoal,
               child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: BlocBuilder<AppBloc, AppState>(
-                  buildWhen: (previous, current) =>
-                      previous.userJourney != current.userJourney,
-                  builder: (context, state) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            getSvgIcon('icon_notification'),
-                            const SizedBox(width: 15),
-                            InkWell(
-                                onTap: () => SettingsScreen.open(context),
-                                child: getSvgIcon('icon_settings')),
-                          ],
-                        ),
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 20,
-                            ),
-                            child: FutureBuilder<String>(
-                              future: _getName(),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasData) {
-                                  return CustomTextNew(
-                                    _getHeaderTitle(context, snapshot.data!),
-                                    style: AskLoraTextStyles.h3.copyWith(
-                                      color: AskLoraColors.white,
-                                    ),
-                                  );
-                                } else {
-                                  return const SizedBox.shrink();
-                                }
-                              },
-                            ),
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          getSvgIcon('icon_notification'),
+                          const SizedBox(width: 15),
+                          InkWell(
+                              onTap: () => SettingsScreen.open(context),
+                              child: getSvgIcon('icon_settings')),
+                        ],
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20,
+                          ),
+                          child: FutureBuilder<String>(
+                            future: _getName(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<String> snapshot) {
+                              if (snapshot.hasData) {
+                                return CustomTextNew(
+                                  _getHeaderTitle(context, snapshot.data!),
+                                  style: AskLoraTextStyles.h3.copyWith(
+                                    color: AskLoraColors.white,
+                                  ),
+                                );
+                              } else {
+                                return const SizedBox.shrink();
+                              }
+                            },
                           ),
                         ),
-                      ],
-                    );
-                  },
-                ),
-              ),
+                      ),
+                    ],
+                  )),
             ),
           ),
         ),
@@ -90,10 +83,10 @@ class CustomSliverPersistentHeader extends SliverPersistentHeaderDelegate {
 
   String _getHeaderTitle(BuildContext context, String name) {
     if (UserJourney.compareUserJourney(
-        context: context, target: UserJourney.learnBotPlank)) {
+        context: context, target: UserJourney.deposit)) {
       return S.of(context).afterPayDepositHeaderTitle;
     } else if (UserJourney.compareUserJourney(
-        context: context, target: UserJourney.deposit)) {
+        context: context, target: UserJourney.kyc)) {
       return S.of(context).beforeDepositHeaderTitle(name);
     } else {
       return S.of(context).beforeKYCHeaderTitle(name);
