a4032e350a77d005e4721eb2f891310e4b4fb70c
wahyu.ramadhan <wahyu@loratechai.com>
2023-07-17 11:48:28 +0700

    update properties in ppi result screen

--- lib/feature/onboarding/ppi/presentation/ppi_result_screen.dart
+++ lib/feature/onboarding/ppi/presentation/ppi_result_screen.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
 
-import '../../../../core/presentation/custom_status_widget.dart';
 import '../../../../core/presentation/custom_text_new.dart';
-import '../../../../core/presentation/lora_memoji_header.dart';
-import '../../../../core/presentation/lora_memoji_widget.dart';
+import '../../../../core/styles/asklora_colors.dart';
 import '../../../../core/styles/asklora_text_styles.dart';
+import '../../../../core/utils/app_icons.dart';
 import '../../../../core/values/app_values.dart';
 
 enum PpiResult { success, failed }
 
 class PpiResultScreen extends StatelessWidget {
-  final String memojiText;
+  final String title;
   final String additionalMessage;
   final Widget bottomButton;
   final Widget? richText;
   final PpiResult? ppiResult;
   final TextStyle? additionalMessageTextStyle;
   final double bottomPadding;
-  final LoraMemojiType loraMemojiType;
 
   const PpiResultScreen(
-      {required this.memojiText,
+      {required this.title,
       required this.ppiResult,
       this.additionalMessage = '',
       required this.bottomButton,
       this.additionalMessageTextStyle,
       this.richText,
       this.bottomPadding = 35,
-      this.loraMemojiType = LoraMemojiType.lora2,
       Key? key})
       : super(key: key);
 
@@ -45,14 +42,22 @@ class PpiResultScreen extends StatelessWidget {
               children: [
                 Column(
                   children: [
-                    if (ppiResult == PpiResult.success)
-                      LoraMemojiHeader(
-                          text: memojiText, loraMemojiType: loraMemojiType)
-                    else
-                      CustomStatusWidget(
-                        title: memojiText,
-                        statusType: StatusType.failed,
-                      ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: getAPngImage(
+                          ppiResult == PpiResult.success
+                              ? 'lora_animation_green'
+                              : 'lora_animation_pink',
+                          width: 208,
+                          fit: BoxFit.fitHeight),
+                    ),
+                    CustomTextNew(
+                      title,
+                      style: AskLoraTextStyles.h4
+                          .copyWith(color: AskLoraColors.charcoal),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 40),
                     if (additionalMessage.isNotEmpty)
                       Padding(
                         padding: const EdgeInsets.symmetric(
