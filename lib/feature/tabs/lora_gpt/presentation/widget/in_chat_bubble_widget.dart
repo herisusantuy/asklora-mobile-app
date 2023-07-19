b64188be6233322f0a36704b3db8922872e19977
Krishna Gupta <krishna.gupta@loratechai.com>
2023-07-19 16:26:49 +0800

    remove border around chat bubble in AI

--- lib/feature/tabs/lora_gpt/presentation/widget/in_chat_bubble_widget.dart
+++ lib/feature/tabs/lora_gpt/presentation/widget/in_chat_bubble_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 import '../../../../../core/presentation/custom_text_new.dart';
@@ -30,31 +28,20 @@ class InChatBubbleWidget extends StatelessWidget {
           children: [
             const SizedBox(width: 30),
             Flexible(
-              child: ClipRect(
-                child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 7,
-                      sigmaY: 7,
+                child: Container(
+                    padding: const EdgeInsets.all(15),
+                    margin: const EdgeInsets.only(bottom: 5, left: 10),
+                    decoration: BoxDecoration(
+                      color: AskLoraColors.white.withOpacity(0.2),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
                     ),
-                    child: Container(
-                        padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(bottom: 5),
-                        decoration: BoxDecoration(
-                          color: AskLoraColors.white.withOpacity(0.2),
-                          border: Border.all(
-                              width: 1.5,
-                              color: AskLoraColors.white.withOpacity(0.8)),
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ),
-                        ),
-                        child: SelectableText(message,
-                            style: AskLoraTextStyles.body2
-                                .copyWith(color: AskLoraColors.white)))),
-              ),
-            ),
+                    child: SelectableText(message,
+                        style: AskLoraTextStyles.body2
+                            .copyWith(color: AskLoraColors.white)))),
           ],
         )
       ],
