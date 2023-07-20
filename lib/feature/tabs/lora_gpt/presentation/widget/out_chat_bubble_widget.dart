28c23b0b5620f3fbe20967dbd54129d48247513f
Kang Hyunwoo <shawnkang0827@outlook.com>
2023-07-20 12:33:13 +0800

    Update out_chat_bubble_widget.dart

--- lib/feature/tabs/lora_gpt/presentation/widget/out_chat_bubble_widget.dart
+++ lib/feature/tabs/lora_gpt/presentation/widget/out_chat_bubble_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../../core/presentation/animated_text.dart';
 import '../../../../../core/presentation/custom_text_new.dart';
-import '../../../../../core/presentation/typer_animated_text.dart';
 import '../../../../../core/styles/asklora_colors.dart';
 import '../../../../../core/styles/asklora_text_styles.dart';
 import '../../../../../core/utils/app_icons.dart';
