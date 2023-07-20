3ad2a78edfca1094d58ae3b7c4d711dd13820b05
Krishna Gupta <krishna.gupta@loratechai.com>
2023-07-20 12:04:11 +0800

    Lora Chat screen revamp

--- lib/feature/tabs/lora_gpt/presentation/lora_ai_overlay_screen.dart
+++ lib/feature/tabs/lora_gpt/presentation/lora_ai_overlay_screen.dart
@@ -8,46 +8,53 @@ class LoraAiOverlayScreen extends StatelessWidget {
     return BlocBuilder<LoraGptBloc, LoraGptState>(
       buildWhen: (previous, current) => previous != current,
       builder: (context, state) {
-        return CustomStretchedLayout(
-          header: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [const SizedBox(height: 40), loraGreenAnimation]),
-          content:
-              Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            CustomTextNew(
-              S.of(context).askloraYouUltimateFinancialAdvisor,
-              style: AskLoraTextStyles.h3.copyWith(color: AskLoraColors.white),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            CustomTextNew(S.of(context).askMeAnythingRelatedToFinance,
-                style: AskLoraTextStyles.body1
-                    .copyWith(color: AskLoraColors.white)),
-          ]),
-          bottomButton: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              BreathingGlowingButton(
-                height: 75.0,
-                width: 75.0,
-                onTap: () {
-                  context.read<LoraGptBloc>().add(
-                      const ShowOverLayScreen(shouldShowOverlayScreen: false));
-                },
-                buttonBackgroundColor: const Color(0xFF373A49),
-                glowColor: Colors.white.withAlpha(100),
-                child: Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.white, width: 1.5)),
-                    child: getSvgIcon('icon_sent_text',
-                        color: AskLoraColors.white, fit: BoxFit.none)),
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40),
+            loraGreenAnimation,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  CustomTextNew(
+                    S.of(context).askloraYouUltimateFinancialAdvisor,
+                    style: AskLoraTextStyles.h3
+                        .copyWith(color: AskLoraColors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  CustomTextNew(S.of(context).askMeAnythingRelatedToFinance,
+                      style: AskLoraTextStyles.body1
+                          .copyWith(color: AskLoraColors.white)),
+                  Expanded(
+                      child: Center(
+                    child: GlowingButton(
+                      height: 68.0,
+                      width: 68.0,
+                      onTap: () {
+                        context.read<LoraGptBloc>().add(const ShowOverLayScreen(
+                            shouldShowOverlayScreen: false));
+                      },
+                      buttonBackgroundColor: const Color(0xFF373A49),
+                      glowColor: Colors.white.withAlpha(100),
+                      child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  color: Colors.transparent, width: 0)),
+                          child: getSvgIcon('icon_start_ai_chat',
+                              color: AskLoraColors.white, fit: BoxFit.none)),
+                    ),
+                  ))
+                ],
               ),
-              const SizedBox(height: 40),
-            ],
-          ),
+            ),
+          ],
         );
       },
     );
