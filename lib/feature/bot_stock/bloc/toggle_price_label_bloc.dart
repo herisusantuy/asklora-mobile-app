47ff0fd3fb776250d82ef3168e25f3d01db2a116
Kang Hyunwoo <shawnkang0827@outlook.com>
2023-07-21 10:23:05 +0800

    Update toggle_price_label_bloc.dart

--- lib/feature/bot_stock/bloc/toggle_price_label_bloc.dart
+++ lib/feature/bot_stock/bloc/toggle_price_label_bloc.dart
@@ -3,20 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'toggle_price_label_event.dart';
 part 'toggle_price_label_state.dart';
 
-
 class ToggleTextBloc extends Bloc<ToggleEvent, ToggleState> {
   ToggleTextBloc() : super(ShowPriceDifferenceState(false)) {
     // Register the event handler
     on<TogglePriceDifferenceEvent>(
-        (event, emit) => _mapTogglePriceDifferenceEventToState(emit));
+        (event, emit) => _mapTogglePriceDifferenceEventToState(event, emit));
 
     // Load the user's saved choice on Bloc initialization
     _loadUserChoice();
   }
 
-  void _mapTogglePriceDifferenceEventToState(Emitter<ToggleState> emit) {
-    final currentState = state;
-    final newState = ShowPriceDifferenceState(!currentState.showPriceDifference);
+  void _mapTogglePriceDifferenceEventToState(
+      TogglePriceDifferenceEvent event, Emitter<ToggleState> emit) {
+    final showPriceDifference =
+        event.showPriceDifference ?? !state.showPriceDifference;
+    final newState = ShowPriceDifferenceState(showPriceDifference);
     emit(newState);
 
     // Save the user's choice
@@ -28,17 +29,17 @@ class ToggleTextBloc extends Bloc<ToggleEvent, ToggleState> {
     final choice = await getUserChoice();
     add(TogglePriceDifferenceEvent(showPriceDifference: choice));
   }
-}
 
-// Save the user's choice
-Future<void> saveUserChoice(bool showPriceDifference) async {
-  final prefs = await SharedPreferences.getInstance();
-  prefs.setBool('showPriceDifference', showPriceDifference);
-}
+  // Save the user's choice
+  Future<void> saveUserChoice(bool showPriceDifference) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('showPriceDifference', showPriceDifference);
+  }
 
 // Retrieve the user's choice
-Future<bool> getUserChoice() async {
-  final prefs = await SharedPreferences.getInstance();
-  // Return the user's saved choice, or false as default
-  return prefs.getBool('showPriceDifference') ?? false;
+  Future<bool> getUserChoice() async {
+    final prefs = await SharedPreferences.getInstance();
+    // Return the user's saved choice, or false as default
+    return prefs.getBool('showPriceDifference') ?? false;
+  }
 }
