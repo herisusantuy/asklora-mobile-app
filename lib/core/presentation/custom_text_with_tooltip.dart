fa2e90110a979cbafc9c11384df9328b799b73ca
askloraheri <heri.susanto@loratechai.com>
2023-07-24 14:08:44 +0700

    Resolve custom text with tooltip based on comment

--- lib/core/presentation/custom_text_with_tooltip.dart
+++ lib/core/presentation/custom_text_with_tooltip.dart
@@ -7,65 +7,58 @@ import '../../../../core/utils/app_icons.dart';
 
 class CustomTextWithTooltip extends StatelessWidget {
   final String title;
-  final String? tooltipText;
+  final String tooltipText;
   final TextStyle? titleStyle;
 
   const CustomTextWithTooltip(
-      {required this.title, this.tooltipText, this.titleStyle, Key? key})
+      {required this.title,
+      required this.tooltipText,
+      this.titleStyle,
+      Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible(
-              child: CustomTextNew(
-                title,
-                style: titleStyle ??
-                    AskLoraTextStyles.body4
-                        .copyWith(color: AskLoraColors.charcoal),
-              ),
-            ),
-            if (tooltipText != null)
-              Padding(
-                padding: const EdgeInsets.only(left: 6.0),
-                child: Tooltip(
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 0.6,
-                            blurRadius: 7,
-                            offset: const Offset(
-                                0, 3), // changes position of shadow
-                          ),
-                        ],
-                        color: AskLoraColors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        border:
-                            Border.all(color: AskLoraColors.primaryMagenta)),
-                    triggerMode: TooltipTriggerMode.tap,
-                    showDuration: const Duration(seconds: 10),
-                    richMessage: WidgetSpan(
-                        alignment: PlaceholderAlignment.baseline,
-                        baseline: TextBaseline.alphabetic,
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                              maxWidth:
-                                  MediaQuery.of(context).size.width * 2 / 3),
-                          child: CustomTextNew(tooltipText!,
-                              style: AskLoraTextStyles.body3
-                                  .copyWith(color: AskLoraColors.charcoal)),
-                        )),
-                    padding: const EdgeInsets.all(10),
-                    child: getSvgIcon('icon_info')),
-              )
-          ],
+        Flexible(
+          child: CustomTextNew(
+            title,
+            style: titleStyle ??
+                AskLoraTextStyles.body4.copyWith(color: AskLoraColors.charcoal),
+          ),
         ),
+        Padding(
+          padding: const EdgeInsets.only(left: 6.0),
+          child: Tooltip(
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 0.6,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                  color: AskLoraColors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: AskLoraColors.primaryMagenta)),
+              triggerMode: TooltipTriggerMode.tap,
+              showDuration: const Duration(seconds: 10),
+              richMessage: WidgetSpan(
+                  alignment: PlaceholderAlignment.baseline,
+                  baseline: TextBaseline.alphabetic,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 2 / 3),
+                    child: CustomTextNew(tooltipText,
+                        style: AskLoraTextStyles.body3
+                            .copyWith(color: AskLoraColors.charcoal)),
+                  )),
+              padding: const EdgeInsets.all(10),
+              child: getSvgIcon('icon_info')),
+        )
       ],
     );
   }
