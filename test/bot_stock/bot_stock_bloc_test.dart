3723f95db08d4c52db8afd0327ac58756764363a
Krishna Gupta <krishna.gupta@loratechai.com>
2023-07-27 12:44:06 +0800

    fixed test caese

--- test/bot_stock/bot_stock_bloc_test.dart
+++ test/bot_stock/bot_stock_bloc_test.dart
@@ -46,7 +46,8 @@ void main() async {
         BaseResponse.error();
 
     const BotRecommendationModel botRecommendationModel =
-        BotRecommendationModel(1, '', '', '', 'Pullup', '', '', '', '');
+        BotRecommendationModel(
+            1, '', '', '', 'Pullup', '', '', '', '', '', '', '');
 
     final BaseResponse<TransactionLedgerBalanceResponse> ledgerBalanceResponse =
         BaseResponse.complete(
