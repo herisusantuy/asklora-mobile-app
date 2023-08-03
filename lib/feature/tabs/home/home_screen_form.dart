c624870ad459a361a4072cabd6f435c292dc5eed
wahyu.ramadhan <wahyu@loratechai.com>
2023-08-03 15:13:38 +0700

    remove unused import file

--- lib/feature/tabs/home/home_screen_form.dart
+++ lib/feature/tabs/home/home_screen_form.dart
@@ -17,7 +17,6 @@ import '../../../core/styles/asklora_text_styles.dart';
 import '../../../core/utils/extensions.dart';
 import '../../../generated/l10n.dart';
 import '../../ai/investment_style_question/presentation/ai_investment_style_question_welcome_screen.dart';
-import '../../bot_stock/domain/bot_recommendation_model.dart';
 import '../../bot_stock/presentation/gift/gift_bot_stock_welcome_screen.dart';
 import '../../bot_stock/utils/bot_stock_utils.dart';
 import '../../onboarding/kyc/presentation/kyc_screen.dart';
@@ -28,21 +27,13 @@ import 'custom_header/custom_sliver_persistent_header.dart';
 import 'widgets/on_boarding_status/on_boarding_status.dart';
 
 part 'widgets/home_screen_content_widget.dart';
-
 part 'widgets/home_screen_free_bot_stock_timer_widget.dart';
-
 part 'widgets/home_screen_horizontal_padding_widget.dart';
-
 part 'widgets/home_screen_investment_style_widget.dart';
-
 part 'widgets/home_screen_milestone_completion_reminder_widget.dart';
-
 part 'widgets/home_screen_milestone_completion_widget.dart';
-
 part 'widgets/home_screen_need_help_button_widget.dart';
-
 part 'widgets/home_screen_pop_up_message_widget.dart';
-
 part 'widgets/home_screen_pop_up_message_with_bot_badge_widget.dart';
 
 class HomeScreenForm extends StatelessWidget {
