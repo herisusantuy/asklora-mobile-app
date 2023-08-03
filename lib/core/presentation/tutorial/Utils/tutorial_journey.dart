0efc199d292b54b84a6ae7f220551428ede58d89
wahyu.ramadhan <wahyu@loratechai.com>
2023-08-03 11:02:12 +0700

    AL1-3270 : - Tutorials  REVAMPED

--- lib/core/presentation/tutorial/Utils/tutorial_journey.dart
+++ lib/core/presentation/tutorial/Utils/tutorial_journey.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TutorialJourney {
+  static GlobalKey tickerDetails = GlobalKey();
   static GlobalKey botDetails = GlobalKey();
+  static GlobalKey tellMeMoreButton = GlobalKey();
   static GlobalKey botChart = GlobalKey();
   static GlobalKey chatLoraTab = GlobalKey();
   static GlobalKey summaryTrade = GlobalKey();
--- lib/core/presentation/tutorial/custom_show_case_view.dart
+++ lib/core/presentation/tutorial/custom_show_case_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:showcaseview/showcaseview.dart';
 
+import '../../../generated/l10n.dart';
 import '../../styles/asklora_colors.dart';
-import 'tutorial_touch_guide.dart';
+import '../../styles/asklora_text_styles.dart';
+import '../custom_text_new.dart';
 
 enum PointerPosition {
   top,
@@ -17,6 +19,7 @@ class CustomShowcaseView extends StatelessWidget {
   final TooltipPosition tooltipPosition;
   final BorderRadius targetBorderRadius;
   final EdgeInsets targetPadding;
+  final EdgeInsets margin;
 
   final double overlayOpacity;
   final Color overlayColor;
@@ -31,8 +34,9 @@ class CustomShowcaseView extends StatelessWidget {
     this.targetPadding = const EdgeInsets.all(10),
     this.targetBorderRadius = const BorderRadius.all(Radius.circular(20)),
     this.overlayColor = Colors.black,
-    this.overlayOpacity = 0.5,
+    this.overlayOpacity = 0.8,
     this.pointerPosition = PointerPosition.bottom,
+    this.margin = const EdgeInsets.only(top: 10),
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -54,20 +58,25 @@ class CustomShowcaseView extends StatelessWidget {
         child: Container(
           color: Colors.transparent,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               if (pointerPosition == PointerPosition.top)
                 Padding(
                   padding: const EdgeInsets.only(bottom: 10),
                   child: GestureDetector(
-                      onTap: onToolTipClick, child: const TutorialTouchGuide()),
+                    onTap: onToolTipClick,
+                    child: CustomTextNew(
+                      S.of(context).tapAnyWhere,
+                      style: AskLoraTextStyles.subtitle2
+                          .copyWith(color: AskLoraColors.white),
+                    ),
+                  ),
                 ),
               GestureDetector(
                 onTap: onToolTipClick,
                 child: Container(
                     margin: tooltipPosition == TooltipPosition.top
                         ? EdgeInsets.zero
-                        : const EdgeInsets.only(top: 10),
+                        : margin,
                     width: MediaQuery.of(context).size.width -
                         30, //* width of tooltip widget
                     padding: const EdgeInsets.symmetric(
@@ -79,13 +88,16 @@ class CustomShowcaseView extends StatelessWidget {
                     child: tooltipWidget),
               ),
               if (pointerPosition == PointerPosition.bottom)
-                Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                        onTap: onToolTipClick,
-                        child: const TutorialTouchGuide()),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: GestureDetector(
+                    onTap: onToolTipClick,
+                    child: CustomTextNew(
+                      'Tap anywhere to continue',
+                      style: AskLoraTextStyles.subtitle2
+                          .copyWith(color: AskLoraColors.white),
+                    ),
+                  ),
                 )
             ],
           ),
