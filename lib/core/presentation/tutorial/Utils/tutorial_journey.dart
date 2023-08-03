d50f5e0b7bb08e54f27192a21dd23d1b386fefe4
wahyu.ramadhan <wahyu@loratechai.com>
2023-08-03 15:39:10 +0700

    commit code

--- lib/core/presentation/tutorial/Utils/tutorial_journey.dart
+++ lib/core/presentation/tutorial/Utils/tutorial_journey.dart
@@ -5,10 +5,7 @@ class TutorialJourney {
   static GlobalKey botDetails = GlobalKey();
   static GlobalKey tellMeMoreButton = GlobalKey();
   static GlobalKey botChart = GlobalKey();
-  static GlobalKey chatLoraTab = GlobalKey();
   static GlobalKey summaryTrade = GlobalKey();
-  static GlobalKey sendQuestionButton = GlobalKey();
-  static GlobalKey chatLoraButton = GlobalKey();
 
   ///bot recommendation
   static GlobalKey botRecommendationList = GlobalKey();
