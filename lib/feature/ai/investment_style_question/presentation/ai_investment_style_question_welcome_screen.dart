475d3d02855cd4f5efb8aa80089839b41aa38588
javasusanto <heri.susanto@loratechai.com>
2023-08-14 08:33:07 +0700

    add Press back again to exit Asklora in AiInvestmentStyleQuestionWelcomeScreen

--- lib/feature/ai/investment_style_question/presentation/ai_investment_style_question_welcome_screen.dart
+++ lib/feature/ai/investment_style_question/presentation/ai_investment_style_question_welcome_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/presentation/custom_in_app_notification.dart';
+import '../../../../core/utils/storage/shared_preference.dart';
 import '../../../../generated/l10n.dart';
+import '../../../onboarding/ppi/presentation/investment_style_question/isq/bloc/isq_onboarding_bloc.dart';
 import '../../../tabs/for_you/investment_style/presentation/ai_investment_style_question_for_you_screen.dart';
 import '../../presentation/ai_welcome_screen.dart';
 import '../../presentation/widgets/ai_welcome_subtitle_text.dart';
@@ -20,27 +25,60 @@ class AiInvestmentStyleQuestionWelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AiWelcomeScreen(
-      enableBackgroundImage: isqType == ISQType.onboarding,
-      aiThemeType: aiThemeType,
-      title: S.of(context).timeForInvestmentStyleQuestion,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 32),
-        child: AiWelcomeSubtitleText(
-          aiThemeType: aiThemeType,
-          subTitle: S.of(context).isqWillHelpMeUnderstandWhatKindOfStocks,
-        ),
+    return BlocProvider(
+      create: (context) =>
+          IsqOnBoardingBloc(sharedPreference: SharedPreference()),
+      child: BlocConsumer<IsqOnBoardingBloc, IsqOnBoardingState>(
+        listener: isqOnboardingBlocListener,
+        listenWhen: isqOnboardingBlocListenWhen,
+        builder: (context, state) {
+          return WillPopScope(
+            onWillPop: () async {
+              context.read<IsqOnBoardingBloc>().add(BackButtonClicked());
+              return false;
+            },
+            child: AiWelcomeScreen(
+              enableBackgroundImage: isqType == ISQType.onboarding,
+              aiThemeType: aiThemeType,
+              title: S.of(context).timeForInvestmentStyleQuestion,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: AiWelcomeSubtitleText(
+                  aiThemeType: aiThemeType,
+                  subTitle:
+                      S.of(context).isqWillHelpMeUnderstandWhatKindOfStocks,
+                ),
+              ),
+              onBottomButtonTap: () {
+                if (isqType == ISQType.onboarding) {
+                  AiInvestmentStyleQuestionOnboardingScreen.open(context);
+                } else {
+                  AiInvestmentStyleQuestionForYouScreen.open(context,
+                      aiThemeType: aiThemeType);
+                }
+              },
+            ),
+          );
+        },
       ),
-      onBottomButtonTap: () {
-        if (isqType == ISQType.onboarding) {
-          AiInvestmentStyleQuestionOnboardingScreen.open(context);
-        } else {
-          AiInvestmentStyleQuestionForYouScreen.open(context,
-              aiThemeType: aiThemeType);
-        }
-      },
     );
   }
+
+  void isqOnboardingBlocListener(
+      BuildContext context, IsqOnBoardingState state) {
+    if (state.isqOnBoardingBackState ==
+        IsqOnBoardingBackState.openConfirmation) {
+      CustomInAppNotification.show(context, S.of(context).pressBackAgain,
+          type: PopupType.grey);
+    } else if (state.isqOnBoardingBackState ==
+        IsqOnBoardingBackState.closeApp) {
+      SystemNavigator.pop();
+    }
+  }
+
+  bool isqOnboardingBlocListenWhen(
+          IsqOnBoardingState previous, IsqOnBoardingState current) =>
+      previous.isqOnBoardingBackState != current.isqOnBoardingBackState;
 
   static void open(BuildContext context) =>
       Navigator.of(context, rootNavigator: true).pushNamed(route);
--- lib/feature/onboarding/ppi/presentation/investment_style_question/isq/bloc/isq_onboarding_state.dart
+++ lib/feature/onboarding/ppi/presentation/investment_style_question/isq/bloc/isq_onboarding_state.dart
@@ -1,22 +1,33 @@
 part of 'isq_onboarding_bloc.dart';
 
+enum IsqOnBoardingBackState { none, openConfirmation, closeApp }
+
 class IsqOnBoardingState extends Equatable {
   final bool aiWelcomeScreenStatus;
   final ResponseState isqOnboardingResponseState;
+  final IsqOnBoardingBackState isqOnBoardingBackState;
 
   const IsqOnBoardingState(
       {this.aiWelcomeScreenStatus = false,
-      this.isqOnboardingResponseState = ResponseState.unknown})
+      this.isqOnboardingResponseState = ResponseState.unknown,
+      this.isqOnBoardingBackState = IsqOnBoardingBackState.none})
       : super();
 
   IsqOnBoardingState copyWith(
       {bool aiWelcomeScreenStatus = false,
-      ResponseState isqOnboardingResponseState = ResponseState.unknown}) {
+      ResponseState isqOnboardingResponseState = ResponseState.unknown,
+      IsqOnBoardingBackState isqOnBoardingBackState =
+          IsqOnBoardingBackState.none}) {
     return IsqOnBoardingState(
         aiWelcomeScreenStatus: aiWelcomeScreenStatus,
-        isqOnboardingResponseState: isqOnboardingResponseState);
+        isqOnboardingResponseState: isqOnboardingResponseState,
+        isqOnBoardingBackState: isqOnBoardingBackState);
   }
 
   @override
-  List<Object> get props => [aiWelcomeScreenStatus, isqOnboardingResponseState];
+  List<Object> get props => [
+        aiWelcomeScreenStatus,
+        isqOnboardingResponseState,
+        isqOnBoardingBackState
+      ];
 }
